51e70c0dbd9a7cd0a70622e07ba0588b99d301a9
Chen Xiaocong <37326370+cxcacac@users.noreply.github.com>
2021-04-23 23:13:46 +0800

    adding descriptions

--- cpp_version/utilities/IORecord.cpp
+++ cpp_version/utilities/IORecord.cpp
@@ -24,6 +24,13 @@ int main(){
     return 0;
 }
 
+/*
+ ***********************************************************************
+ * for integers, using scanf or cin to read data till 
+ * (1) scanf("%d", &num)==false, cin>>num==false, which can be judged by while.
+ * (2) or cin>>size first, then use while(size--) to read 
+ ***********************************************************************
+*/
 void integerIO(){
     // input: a b
     // output: a+b
@@ -72,52 +79,59 @@ void integerIO(){
         cout << sum << endl;
     }
 }
-    void stringIO(){
-        // n
-        // abc acd .... (n blanks) dfd
-        // must not mix cin with getline
-        int num;
-        cin >> num;
+/*
+ ***********************************************************************
+ * for strings, using getline(cin, output) to get each test case, return true if not null.
+ * using istringstream to get each string into vector<string>.
+ * istringstream read string like cin, use blank/tab/enter as end character.
+ ***********************************************************************
+*/
+void stringIO(){
+    // n
+    // abc acd .... (n blanks) dfd
+    // must not mix cin with getline
+    int num;
+    cin >> num;
+    vector<string> v;
+    string val;
+    while(num--){
+        cin >> val;
+        v.push_back(val);
+    }
+    sort(v.begin(), v.end());
+    unsigned long len = v.size();
+    for (int i = 0; i < len; i++){
+        cout << v[i] << ' ';
+    }
+    cout << v.back()<<endl;
+
+    // abc adc dfa dfa dfdf
+    string input;
+    while(getline(cin, input)){
+        istringstream iss(input);
+        string s;
         vector<string> v;
-        string val;
-        while(num--){
-            cin >> val;
-            v.push_back(val);
+        while(iss>>s){
+            v.push_back(s);
         }
         sort(v.begin(), v.end());
         unsigned long len = v.size();
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < len - 1; i++){
             cout << v[i] << ' ';
         }
-        cout << v.back()<<endl;
+        cout << v.back() << endl;
+    }
 
-        // abc adc dfa dfa dfdf
-        string input;
-        while(getline(cin, input)){
-            istringstream iss(input);
-            string s;
-            vector<string> v;
-            while(iss>>s){
-                v.push_back(s);
-            }
-            sort(v.begin(), v.end());
-            unsigned long len = v.size();
-            for (int i = 0; i < len - 1; i++){
-                cout << v[i] << ' ';
-            }
-            cout << v.back() << endl;
-        }
-
-        // abc,add,bcd,add
-        // every input : every test case;
-        string input;
-        while(getline(cin, input)){
-            istringstream iss(input);
-            string s;
-            vector<string> v;
-            while(getline(iss, s, ',')){
-                v.push_back(s);
-            }
+    // abc,add,bcd,add
+    // every input : every test case;
+    string input;
+    while(getline(cin, input)){
+        istringstream iss(input);
+        string s;
+        vector<string> v;
+        while(getline(iss, s, ',')){
+            v.push_back(s);
         }
     }
+}
 
